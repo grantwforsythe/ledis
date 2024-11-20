e75f9915a2acfcf59d50f5d6ec6d7bea7a8910c9
Grant Forsythe <grantwforsythe@gmail.com>
2024-11-19 20:45:42 -0500

    feat: pong pong

--- app/server.go
+++ app/server.go
@@ -10,29 +10,43 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	ln, err := net.Listen("tcp", "0.0.0.0:6379")
+	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := ln.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		defer listener.Close()
+
+		err = handleConnection(conn)
+		if err != nil {
+			fmt.Println("Error handling connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
 
+}
+
+func handleConnection(conn net.Conn) error {
 	defer conn.Close()
 
-	payload := make([]byte, 128)
-	_, err = conn.Read(payload)
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Failed to read incoming client request")
-		os.Exit(1)
+		return err
 	}
 
 	_, err = conn.Write([]byte("+PONG\r\n"))
 	if err != nil {
-		fmt.Println("Failed to write 'PONG' to the connection")
-		os.Exit(1)
+		return err
 	}
+
+	return nil
 }
