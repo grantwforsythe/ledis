e794fd19be455087d68290e89f771e9a356504a4
Grant Forsythe <grantwforsythe@gmail.com>
2024-11-19 21:08:28 -0500

    fix: do not create a new conn on each iteration

--- app/server.go
+++ app/server.go
@@ -7,9 +7,6 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	listener, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
@@ -18,32 +15,29 @@ func main() {
 
 	defer listener.Close()
 
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 
-		err = handleConnection(conn)
+	defer conn.Close()
+
+	for {
+		err = handlePing(conn)
 		if err != nil {
 			fmt.Println("Error handling connection: ", err.Error())
 			os.Exit(1)
 		}
 	}
-
 }
 
-func handleConnection(conn net.Conn) error {
-	defer conn.Close()
-
+func handlePing(conn net.Conn) error {
 	buf := make([]byte, 1024)
-	n, err := conn.Read(buf)
+	_, err := conn.Read(buf)
 	if err != nil {
 		return err
 	}
-
-	fmt.Println("Recieved: ", buf[:n])
 
 	_, err = conn.Write([]byte("+PONG\r\n"))
 	if err != nil {
